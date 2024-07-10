66ed1a81330825ab09a426065979375be76bcd1c
WebShell1998 <43918790+WebShell1998@users.noreply.github.com>
2024-07-11 01:00:08 +0500

    Initial Commit

---
+++ PreviewTest/PreviewTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreviewTestApp.swift
+//  PreviewTest
+//
+//  Created by Николай Щербаков on 11.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct PreviewTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
